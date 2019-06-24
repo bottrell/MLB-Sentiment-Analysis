f42d8273e09a2f7307c0f75555340c667b11334a
Jordan <bottrell@umich.edu>
2019-06-24 14:27:09 -0400

    added skeleton

---
+++ scrape.py
@@ -0,0 +1,8 @@
+import tweepy
+import secrets
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ teams.py
@@ -0,0 +1,148 @@
+#potentially add notable players for each team (potentially time consuming)
+teaminfo = {
+    {
+        "team_name" : "Braves",
+        "abbrev" : "ATL",
+        "location" : "Atlanta"
+    },
+    {
+        "team_name" : "Diamondbacks",
+        "abbrev" : "ARI",
+        "location" : "Arizona"
+    },
+    {
+        "team_name" : "Orioles",
+        "abbrev" : "BAL",
+        "location" : "Baltimore"
+    },
+    {
+        "team_name" : "Red Sox",
+        "abbrev" : "BOS",
+        "location" : "Boston"
+    },
+    {
+        "team_name" : "Cubs",
+        "abbrev" : "CHC",
+        "location" : "Chicago"
+    },
+    {
+        "team_name" : "White Sox",
+        "abbrev" : "CWS",
+        "location" : "Chicago"
+    },
+    {
+        "team_name" : "Reds",
+        "abbrev" : "CIN",
+        "location" : "Cincinnati"
+    },
+    {
+        "team_name" : "Indians",
+        "abbrev" : "CLE",
+        "location" : "Cleveland"
+    },
+    {
+        "team_name" : "Rockies",
+        "abbrev" : "COL",
+        "location" : "Colorado"
+    },
+    {
+        "team_name" : "Tigers",
+        "abbrev" : "DET",
+        "location" : "Detroit"
+    },
+    {
+        "team_name" : "Marlins",
+        "abbrev" : "MIA",
+        "location" : "Miami"
+    },
+    {
+        "team_name" : "Astros",
+        "abbrev" : "HOU",
+        "location" : "Houston"
+    },
+    {
+        "team_name" : "Angels",
+        "abbrev" : "LAA",
+        "location" : "Los Angeles"
+    },
+    {
+        "team_name" : "Dodgers",
+        "abbrev" : "LAD",
+        "location" : "Los Angeles"
+    },
+    {
+        "team_name" : "Brewers",
+        "abbrev" : "Milwaukee",
+        "location" : "MIL"
+    },
+    {
+        "team_name" : "Twins",
+        "abbrev" : "MIN",
+        "location" : "Minnesota"
+    },
+    {
+        "team_name" : "Mets",
+        "abbrev" : "NYM",
+        "location" : "New York"
+    },
+    {
+        "team_name" : "Yankees",
+        "abbrev" : "NYY",
+        "location" : "New York"
+    },
+    {
+        "team_name" : "Athletics",
+        "abbrev" : "OAK",
+        "location" : "Oakland"
+    },
+    {
+        "team_name" : "Phillies",
+        "abbrev" : "PHI",
+        "location" : "Philadelphia"
+    },
+    {
+        "team_name" : "Pirates",
+        "abbrev" : "PIT",
+        "location" : "Pittsburgh"
+    },
+    {
+        "team_name" : "Padres",
+        "abbrev" : "SD",
+        "location" : "San Diego"
+    },
+    {
+        "team_name" : "Mariners",
+        "abbrev" : "SEA",
+        "location" : "Seattle"
+    },
+    {
+        "team_name" : "Giants",
+        "abbrev" : "SF",
+        "location" : "San Fransisco"
+    },
+    {
+        "team_name" : "Cardinals",
+        "abbrev" : "STL",
+        "location" : "St. Louis"
+    },
+    {
+        "team_name" : "Rays",
+        "abbrev" : "TB",
+        "location" : "Tampa Bay"
+    },
+    {
+        "team_name" : "Rangers",
+        "abbrev" : "TEX",
+        "location" : "Texas"
+    },
+    {
+        "team_name" : "Blue Jays",
+        "abbrev" : "TOR",
+        "location" : "Toronto"
+    },
+    {
+        "team_name" : "Nationals",
+        "abbrev" : "WSH",
+        "location" : "Washington"
+    }
+}
